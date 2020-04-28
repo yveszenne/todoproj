7c0f1415eed0180b910c5153bb5caff7c4c28f00
yvesespinosa <yveszenne@gmail.com>
2020-04-28 12:09:07 +0800

    finish app

--- todo_list/views.py
+++ todo_list/views.py
@@ -22,3 +22,31 @@ def delete(request, list_id):
 	item.delete()
 	messages.success(request, ('Item has been deleted!'))
 	return redirect('home')
+
+def cross_off(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = True
+	item.save()
+	return redirect('home')
+
+def uncross(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = False	
+	item.save()
+	return redirect('home')
+
+def edit (request, list_id):
+	if request.method == 'POST':	
+		item = List.objects.get(pk=list_id)
+
+		form = ListForm(request.POST or None, instance=item)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Item has been edited'))
+			return redirect('home')
+	else:
+		item = List.objects.get(pk=list_id)
+		return render(request, 'edit.html', {'item': item})
+
+
